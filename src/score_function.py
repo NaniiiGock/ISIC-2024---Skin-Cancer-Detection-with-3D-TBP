a5607ee22bdae697444ddf18a80909357b87c582
Anastasiia <anastasiia.petrovych@ucu.edu.ua>
2024-12-16 12:07:19 +0200

    Create score_function.py for evaluation

---
+++ src/score_function.py
@@ -0,0 +1,35 @@
+def score(solution: pd.DataFrame, submission: pd.DataFrame, row_id_column_name: str, min_tpr: float = 0.80) -> float:
+    '''
+    2024 ISIC Challenge metric: pAUC
+
+    Given a solution file and submission file, this function returns the
+    the partial area under the receiver operating characteristic (pAUC)
+    above a given true positive rate (TPR) = 0.80.
+    https://en.wikipedia.org/wiki/Partial_Area_Under_the_ROC_Curve.
+
+    (c) 2024 Nicholas R Kurtansky, MSKCC
+
+    Args:
+        solution: ground truth pd.DataFrame of 1s and 0s
+        submission: solution dataframe of predictions of scores ranging [0, 1]
+
+    Returns:
+        Float value range [0, max_fpr]
+    '''
+
+    v_gt = abs(np.asarray(solution.values) - 1)
+    v_pred = -1.0 * np.asarray(submission.values)
+    max_fpr = abs(1 - min_tpr)
+    fpr, tpr, _ = roc_curve(v_gt, v_pred, sample_weight=None)
+    if max_fpr is None or max_fpr == 1:
+        return auc(fpr, tpr)
+    if max_fpr <= 0 or max_fpr > 1:
+        raise ValueError("Expected min_tpr in range [0, 1), got: %r" % min_tpr)
+
+    stop = np.searchsorted(fpr, max_fpr, "right")
+    x_interp = [fpr[stop - 1], fpr[stop]]
+    y_interp = [tpr[stop - 1], tpr[stop]]
+    tpr = np.append(tpr[:stop], np.interp(max_fpr, x_interp, y_interp))
+    fpr = np.append(fpr[:stop], max_fpr)
+    partial_auc = auc(fpr, tpr)
+    return partial_auc
